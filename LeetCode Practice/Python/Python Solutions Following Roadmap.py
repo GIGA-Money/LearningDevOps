55642dc88910d13f02f58f23e169e8e762913320
Chris R <gigachris1@hotmail.com>
2023-05-13 17:19:27 -0400

    Minor formatting.

--- LeetCode Practice/Python/Python Solutions Following Roadmap.py
+++ LeetCode Practice/Python/Python Solutions Following Roadmap.py
@@ -108,8 +108,12 @@ class Solution:
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """
+        @Params: nums: List[int], target: int
+        @Returns: List[int]
+        """
         # hash map
-        seen = {}
+        seen: dict = {}
         for i, num in enumerate(nums):
             complement = target - num
             if complement in seen:
